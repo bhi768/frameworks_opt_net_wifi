4f84950f1b8762593cd79ed660e18af8649c627f
Vinit Deshpande <vinitd@google.com>
2016-01-07 11:22:06 -0800

    Reset idenitities for EAP-SIM networks when SIM is pulled
    This change resets identity and anonymous_identity for all
    networks that use SIM based auth. This ensures that all cached
    data is destroyed when SIM is removed; and newly added SIM
    must be authorized on the network to connect again.
    
    Bug: 23703716
    
    Change-Id: I83d80336b2089c73d99214651b64b79443faf7f7

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -744,6 +744,9 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
     /* alert from firmware */
     static final int CMD_FIRMWARE_ALERT                                 = BASE + 100;
 
+    /* SIM is removed; reset any cached data for it */
+    static final int CMD_RESET_SIM_NETWORKS                             = BASE + 101;
+
     /**
      * Make this timer 40 seconds, which is about the normal DHCP timeout.
      * In no valid case, the WiFiStateMachine should remain stuck in ObtainingIpAddress
@@ -2483,6 +2486,14 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
             setCountryCode(mDefaultCountryCode, /* persist = */ true);
         }
     }
+
+    /**
+     * reset cached SIM credential data
+     */
+    public synchronized void resetSimAuthNetworks() {
+        sendMessage(CMD_RESET_SIM_NETWORKS);
+    }
+
 
     /**
      * Get Network object of current wifi network
@@ -8613,6 +8624,17 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                 case CMD_STOP_RSSI_MONITORING_OFFLOAD:
                     stopRssiMonitoringOffload();
                     break;
+                case CMD_RESET_SIM_NETWORKS:
+                    if (mLastNetworkId != WifiConfiguration.INVALID_NETWORK_ID) {
+                        WifiConfiguration config =
+                                mWifiConfigStore.getWifiConfiguration(mLastNetworkId);
+                        if (mWifiConfigStore.isSimConfig(config)) {
+                            mWifiNative.disconnect();
+                            transitionTo(mDisconnectingState);
+                        }
+                    }
+                    /* allow parent state to reset data for other networks */
+                    return NOT_HANDLED;
                 default:
                     return NOT_HANDLED;
             }
